2635a7db963a57aa9a4cff2cc75fbf5dbbacca95
ichala <alijandhd@gmail.com>
2023-01-01 14:21:25 +0100

    Create Home Page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1 className="text-5xl font-bold uppercase">
+      Ali
+      <span className="hightlight"> Jendoubi</span>
+    </h1>
+    <p className="py-3 text-lg ">Hi, I&lsquo;m Ali Jendoubi (AKA : Chala), A Passionate Self-Taught Full-Stack Software Developer From Tunisia. My Passion For Software Lies With Dreaming Up Ideas And Making Them Come True With Elegant Interfaces. </p>
+    <button type="button" className="btn btn-primary">Get Started</button>
+  </div>
+);
+
+export default Home;
