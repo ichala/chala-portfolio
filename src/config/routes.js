d844d80d0201ff85ff690a0cf9efb75e84da7046
Ali Jendoubi <alijandhd@gmail.com>
2023-01-04 16:02:43 +0100

    Add Skills Children Routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,9 @@ import Contact from '../pages/Contact/Contact';
 import Home from '../pages/Home/Home';
 import About from '../pages/About/About';
 import Layout from './Layout/Layout';
+import Skills from '../pages/Skills/Skills';
+import Frameworks from '../pages/Skills/Frameworks/Frameworks';
+import Experience from '../pages/Skills/Experience/Experience';
 
 const routesConfig = [
   {
@@ -18,6 +21,19 @@ const routesConfig = [
       {
         path: '/about',
         element: <About />,
+      },
+      {
+        element: <Skills />,
+        children: [
+          {
+            path: '/skills',
+            element: <Frameworks />,
+          },
+          {
+            path: '/skills/experience',
+            element: <Experience />,
+          },
+        ],
       },
       {
         path: '*',
