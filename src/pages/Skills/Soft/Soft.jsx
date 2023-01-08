d9faaafadfc462ed1c0926000cf5f1e8a43bcdc2
Ali Jendoubi <alijandhd@gmail.com>
2023-01-08 13:08:59 +0100

    Add soft skills page

---
+++ src/pages/Skills/Soft/Soft.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Soft = () => (
+  <div className="flex flex-wrap gap-2">
+    {/* daisyUi Card */}
+    <div className="card w-full">
+      <div className="card-body">
+        <h2 className="card-title">Soft Skills</h2>
+        <p className="card-subtitle">I have a lot of soft skills</p>
+        <p className="card-text">
+          I have a lot of soft skills
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+export default Soft;
