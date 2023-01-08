ad0cc9f2ebe2eace2d450f749d6382c89fdebcf8
Ali Jendoubi <alijandhd@gmail.com>
2023-01-08 14:35:45 +0100

    Add animation to languages

--- src/components/Skills/SpeakingLanguages.jsx
+++ src/components/Skills/SpeakingLanguages.jsx
@@ -10,48 +10,66 @@ const SpeakingLanguages = () => (
       exit={{ x: -100, opacity: 0 }}
       className="text-lg text-center font-light mt-5 w-full my-4"
     >
-      Languages
+      I Speak
     </motion.h3>
     <div className="flex flex-wrap  p-2 rounded-lg justify-center items-center gap-2">
-      <div className="flex flex-col justify-center items-center gap-2">
+      <motion.div
+        initial={{ x: -100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        exit={{ opacity: 0 }}
+        className="flex flex-col justify-center items-center gap-2"
+      >
         <div
           className="radial-progress text-primary"
           style={{
             '--value': '100',
             '--size': '6rem',
-            '--thickness': '10px',
+            '--thickness': '7px',
           }}
         >
           100%
         </div>
         Arabic
-      </div>
-      <div className="flex flex-col justify-center items-center gap-2">
+      </motion.div>
+      <motion.div
+        initial={{ x: -100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 0.6 }}
+        exit={{ opacity: 0 }}
+        className="flex flex-col justify-center items-center gap-2"
+      >
         <div
           className="radial-progress text-primary"
           style={{
             '--value': '70',
             '--size': '6rem',
-            '--thickness': '10px',
+            '--thickness': '7px',
           }}
         >
           70%
         </div>
         English
-      </div>
-      <div className="flex flex-col justify-center items-center gap-2">
+      </motion.div>
+      <motion.div
+        initial={{ x: -100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 0.7 }}
+        exit={{ opacity: 0 }}
+        className="flex flex-col justify-center items-center gap-2"
+      >
         <div
           className="radial-progress text-primary"
           style={{
             '--value': '50',
             '--size': '6rem',
-            '--thickness': '10px',
+            '--thickness': '7px',
           }}
         >
           50%
         </div>
         French
-      </div>
+      </motion.div>
     </div>
   </div>
 );
