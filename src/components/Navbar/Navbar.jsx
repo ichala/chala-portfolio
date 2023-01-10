3a1acecd80dae13dc9a01d08c7efe6edee4e5bfd
Ali Jendoubi <alijandhd@gmail.com>
2023-01-10 15:54:32 +0100

    Add Projects Link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -20,9 +20,9 @@ const Navbar = ({ children }) => (
             <ul className="menu menu-horizontal">
               <li><NavLink to="/">Home</NavLink></li>
               <li><NavLink to="/about">About</NavLink></li>
+              <li><NavLink to="/projects">Projects</NavLink></li>
               <li><NavLink to="/skills/">Skills</NavLink></li>
               <li><NavLink to="/contact">Contact</NavLink></li>
-
             </ul>
           </div>
         </div>
