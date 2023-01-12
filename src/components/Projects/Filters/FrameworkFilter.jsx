5b8c1f451381ee33aae957a2bb68c7211c84b00b
Ali Jendoubi <alijandhd@gmail.com>
2023-01-12 16:41:04 +0100

    Update Freamework filter

--- src/components/Projects/Filters/FrameworkFilter.jsx
+++ src/components/Projects/Filters/FrameworkFilter.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import IconGenerator from '../../../config/IconGenerator';
+import IconGenerator from '../../../tools/IconGenerator';
 
 const FrameworkFilter = ({ FrameWorks, handleFramework, SelectedFrameworks }) => (
-  FrameWorks.map((framework, index) => (
-    <motion.button
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { delay: index * 0.1 } }}
-      exit={{ opacity: 0 }}
-      onClick={() => handleFramework(framework)}
-      key={framework}
-      type="button"
-      className={`btn btn-sm ${SelectedFrameworks.includes(framework) ? 'btn-primary' : ' btn-active'}`}
-    >
-      {IconGenerator(framework, 20)}
+  <div className="flex flex-wrap gap-2">
+    {FrameWorks.map((framework, index) => (
+      <motion.button
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { delay: index * 0.1 } }}
+        exit={{ opacity: 0 }}
+        onClick={() => handleFramework(framework)}
+        key={framework}
+        type="button"
+        className={`btn btn-sm ${SelectedFrameworks.includes(framework) ? 'btn-primary' : ' btn-active'}`}
+      >
+        {IconGenerator(framework, 20)}
 
-    </motion.button>
-  ))
+      </motion.button>
+
+    )) }
+  </div>
 );
 
 export default FrameworkFilter;
