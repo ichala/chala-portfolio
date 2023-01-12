32ac605bdaf2b88a9a1d186d9b5436f7eabaac45
Ali Jendoubi <alijandhd@gmail.com>
2023-01-12 03:31:54 +0100

    Update firebase route

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import {
-  getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup,
+  getAuth,
 } from 'firebase/auth';
 
 import { getFirestore } from 'firebase/firestore';
@@ -17,8 +17,4 @@ const firebaseConfig = {
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
-const githubProvider = new GithubAuthProvider();
 export const db = getFirestore(app);
-export const signInWithGoogle = () => signInWithPopup(auth, googleProvider);
-export const signInWithGithub = () => signInWithPopup(auth, githubProvider);
