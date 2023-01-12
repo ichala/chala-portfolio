ec7e9291a6a12f97471ed70a6455a271fe9eb07b
Ali Jendoubi <alijandhd@gmail.com>
2023-01-12 16:42:43 +0100

    Updated protected routes

---
+++ src/tools/protected_routes.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { AdminContext } from '../config/Admin/AdminContext';
+
+const Protected = () => {
+  const { currentUser } = useContext(AdminContext);
+  if (!currentUser) {
+    return (
+      <Navigate to={`/login?returnUrl=${window.location.pathname} `} replace />);
+  }
+  return (
+    <Outlet />
+  );
+};
+
+export default Protected;
