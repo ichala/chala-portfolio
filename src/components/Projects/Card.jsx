e457f781f5d089ae5a87b651ea34aeca91537f67
Ali Jendoubi <alijandhd@gmail.com>
2023-01-13 15:21:04 +0100

    Add Action buttons to the cards

--- src/components/Projects/Card.jsx
+++ src/components/Projects/Card.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import { AiFillLock } from 'react-icons/ai';
+import { MdOutlineOpenInNew } from 'react-icons/md';
+import { SiGithub } from 'react-icons/si';
 import IconGenerator from '../../tools/IconGenerator';
 
 const Card = ({ data }) => (
@@ -19,17 +22,36 @@ const Card = ({ data }) => (
     />
     <div className="card-body ">
       <h2 className=" card-title text-base-content italic font-light">{data.details.title}</h2>
-      <div className="flex gap-2 flex-wrap text-primary">
+      <div className="flex gap-2 flex-wrap text-primary items-center justify-start">
         {
         data.details?.frameworks.map((framework) => (
-          <div key={`${framework.value}`}>
-            {IconGenerator(framework.value)}
+          <div key={`${framework.value}`} className="tooltip  tooltip-primary" data-tip={framework.value}>
+            {IconGenerator(framework.value, 20)}
           </div>
         ))
       }
       </div>
       <div className="card-actions justify-end">
-        <button type="button" className="btn btn-primary btn-xs">Learn now!</button>
+        {
+          data.details.private ? (
+            <button type="button" className="btn btn-primary btn-xs gap-2">
+              <AiFillLock />
+              Private
+            </button>
+          ) : (
+            <a href={data.details.github} target="_blank" className="btn btn-primary btn-xs gap-2" rel="noreferrer">
+              <SiGithub />
+              View Code
+
+            </a>
+          )
+        }
+
+        <a href={data.details.live} target="_blank" className="btn btn-primary btn-xs gap-2 " rel="noreferrer">
+          <MdOutlineOpenInNew />
+          View Demo
+
+        </a>
       </div>
     </div>
   </motion.div>
