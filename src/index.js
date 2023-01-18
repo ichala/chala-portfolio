3784f9a29a8e0be10aaeffd9fad55ef224c19c5f
Ali Jendoubi <alijandhd@gmail.com>
2023-01-18 14:58:16 +0100

    Update theme provider

--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AdminContextProvider } from './config/Admin/AdminContext';
 import './config/global.css';
+import { ThemeProvider } from './config/Theme/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
     <BrowserRouter>
-      <AdminContextProvider>
-        <App />
-      </AdminContextProvider>
+
+      <ThemeProvider>
+        <AdminContextProvider>
+          <App />
+        </AdminContextProvider>
+      </ThemeProvider>
+
     </BrowserRouter>
 
   </React.StrictMode>,
